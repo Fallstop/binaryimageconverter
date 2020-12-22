1d063d5cbf9239b19713551c5efef885a5685093
Jasper M-W <jasper@ngita.co.nz>
2020-12-23 10:43:34 +1300

    Now supports RGB input output

--- main.py
+++ main.py
@@ -14,33 +14,39 @@ def image_dimensions(x):
     return (x//center, center)
            
 
-def convertImageToBinary(filename):
-    image = Image.open(filename)
+def convertImageToBinary(args):
+    image = Image.open(args.image)
     imageData = image.tobytes()
     return imageData
 
-def convertBinToImage(filename):
-    with open(filename, "rb") as f:
+def convertBinToImage(args):
+    with open(args.binary, "rb") as f:
         binaryCollections = f.read()
-    dimensions =  image_dimensions(len(binaryCollections))
-    return Image.frombytes('L',dimensions,binaryCollections)
+    if not args.fullColour:
+        print("Using single colour")
+        dimensions =  image_dimensions(len(binaryCollections))
+        image = Image.frombytes('L',dimensions,binaryCollections)
+    else:
+        print("Using colour")
+        dimensions =  image_dimensions(int(len(binaryCollections)/3))
+        print(dimensions)
+        image = Image.frombytes('RGB',dimensions,binaryCollections)
+    return image
 
 
 def main():
-    inputBinary = sys.argv[1]
-    outputImage = sys.argv[2]
-
     parser = argparse.ArgumentParser()
-    parser.add_argument("binary")
-    parser.add_argument("image")
-    parser.add_argument("output")
+    parser.add_argument("binary", help="The path to the binary/data")
+    parser.add_argument("image", help="The path to the image")
+    parser.add_argument("output", help="The direction to convert the data (Either: 'image' or 'binary')")
+    parser.add_argument("-rgb","--fullColour",action="store_true",required=False,help="Store the data in the colour bands, not just greyscale",dest="fullColour")
     args = parser.parse_args()
 
     if args.output == "image":
-        output = convertBinToImage(args.binary)
+        output = convertBinToImage(args)
         output.save(args.image)
     elif args.output == "binary":
-        output = convertImageToBinary(args.image)
+        output = convertImageToBinary(args)
         with open(args.binary,"wb+") as f:
             f.write(output)
 
